275cdf0fbfb422eee0d522157e7c72d6a6b1b5bc
Thimo Grauerholz <thimo.grauerholz@secucloud.com>
2018-03-26 17:20:56 +0200

    changed order of label element

--- src/EventSelector.jsx
+++ src/EventSelector.jsx
@@ -10,15 +10,15 @@ export default function EventSelector({ events, selectedEvent, selectEvent }) {
       <div className="legend-body">
         {events.map(event => (
           <div className="radio-item" key={event.event}>
+            <input
+              type="radio"
+              name="eventSelect"
+              id={`eventSelect${event.event}`}
+              value={event.event}
+              defaultChecked={selectedEvent.event === event.event}
+              onClick={onRadioClick(event)}
+            />
             <label className="radio-label" htmlFor={`eventSelect${event.event}`}>
-              <input
-                type="radio"
-                name="eventSelect"
-                id={`eventSelect${event.event}`}
-                value={event.event}
-                defaultChecked={selectedEvent.event === event.event}
-                onClick={onRadioClick(event)}
-              />
               {event.event}
             </label>
             <div className="legend-color" style={{ backgroundColor: event.color }} />
